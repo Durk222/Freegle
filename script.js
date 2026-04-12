e7c652b206c926bea926a8065c984b79244e0d1d
Franciswow <carvalloalan005@gmail.com>
2026-04-12 17:19:48 -0400

    Función añadida: Cambio de tema

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function toggleTheme() {
+    const body = document.documentElement;
+    const currentTheme = body.getAttribute('data-theme');
+    
+    if (currentTheme === 'dark') {
+        body.setAttribute('data-theme', 'light');
+    } else {
+        body.setAttribute('data-theme', 'dark');
+    }
+}
